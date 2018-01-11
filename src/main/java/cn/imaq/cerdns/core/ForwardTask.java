aefbb75f7fa3ec8c84e1a9ea3a17ef6218082341
Monkey Liang <i@aq741.com>
2018-01-12 00:46:00 +0800

    Use NIO to reduce number of threads

--- src/main/java/cn/imaq/cerdns/core/ForwardTask.java
+++ src/main/java/cn/imaq/cerdns/core/ForwardTask.java
@@ -9,10 +9,15 @@ import org.xbill.DNS.Record;
 import org.xbill.DNS.Section;
 import org.xbill.DNS.Type;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
-import java.util.ArrayList;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -30,72 +35,120 @@ public class ForwardTask implements Runnable {
         log.info("Query: " + reqMessage.getQuestion());
         Message result = null;
         if (reqMessage.getQuestion().getType() == Type.A) {
-            log.info("A type query, use chain");
+            log.info("> A type query, use chain");
             // Use chain
             List<Config.ChainNode> chain = Config.getChain();
-            List<Future<Message>> futures = new ArrayList<>();
-            Message fallback = null;
-            for (Config.ChainNode node : chain) {
-                futures.add(queryPool.submit(new QueryTask(reqMessage, node.getServer(), Config.getTimeout())));
-            }
-            for (int i = 0; i < futures.size(); i++) {
-                Future<Message> future = futures.get(i);
-                log.info("Getting result from server " + chain.get(i).getServer());
-                try {
-                    Message respMessage = future.get();
-                    if (respMessage == null) {
-                        continue;
+            try {
+                long endTime = System.currentTimeMillis() + Config.getTimeout();
+                int size = chain.size();
+                // Send requests
+                Selector selector = Selector.open();
+                DatagramChannel[] channels = new DatagramChannel[size];
+                for (int i = 0; i < size; i++) {
+                    DatagramChannel channel = DatagramChannel.open();
+                    channels[i] = channel;
+                    channel.configureBlocking(false);
+                    channel.connect(chain.get(i).getServer());
+                    channel.register(selector, SelectionKey.OP_READ, i);
+                    channel.socket().setSoTimeout(Config.getTimeout());
+                    channel.write(ByteBuffer.wrap(reqMessage.toWire()));
+                }
+                // Receive responses
+                Message[] responses = new Message[size];
+                ByteBuffer byteBuf = ByteBuffer.allocate(4096);
+                int currentIndex = 0, lastIndex = 0;
+                boolean matched = false;
+                while (true) {
+                    int count = selector.select(endTime - System.currentTimeMillis());
+                    if (count <= 0) {
+                        break;
                     }
-                    fallback = respMessage;
-                    Record[] answer = respMessage.getSectionArray(Section.ANSWER);
-                    if (answer != null && answer.length > 0) {
-                        Config.ChainNode node = chain.get(i);
-                        boolean matches = true;
-                        if (node.getMatchPrefixes() != null) {
-                            for (Record record : answer) {
-                                if (record.getType() == Type.A) {
-                                    boolean match = false;
-                                    int ip = CIDR.toInt(record.rdataToString());
-                                    for (byte len = 0; len < 32; len++) {
-                                        if (node.getMatchPrefixes().contains(new CIDR.Prefix(ip >> (32 - len), len))) {
-                                            match = true;
+                    Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
+                    while (keyIter.hasNext()) {
+                        SelectionKey key = keyIter.next();
+                        keyIter.remove();
+                        if (key.isReadable()) {
+                            int keyIndex = (int) key.attachment();
+                            byteBuf.clear();
+                            ((DatagramChannel) key.channel()).read(byteBuf);
+                            byteBuf.flip();
+                            responses[keyIndex] = new Message(byteBuf);
+                            log.info("Got result from server " + chain.get(keyIndex).getServer());
+                            if (keyIndex >= lastIndex) {
+                                lastIndex = keyIndex;
+                                result = responses[keyIndex];
+                            }
+                        }
+                    }
+                    boolean matches = false;
+                    while (responses[currentIndex] != null && currentIndex < size) {
+                        Message respMessage = responses[currentIndex];
+                        Record[] answer = respMessage.getSectionArray(Section.ANSWER);
+                        if (answer != null && answer.length > 0) {
+                            Config.ChainNode node = chain.get(currentIndex);
+                            matches = true;
+                            if (node.getMatchPrefixes() != null) {
+                                for (Record record : answer) {
+                                    if (record.getType() == Type.A) {
+                                        boolean match = false;
+                                        int ip = CIDR.toInt(record.rdataToString());
+                                        for (byte len = 0; len < 32; len++) {
+                                            if (node.getMatchPrefixes().contains(new CIDR.Prefix(ip >> (32 - len), len))) {
+                                                match = true;
+                                                break;
+                                            }
+                                        }
+                                        if (!match) {
+                                            matches = false;
                                             break;
                                         }
                                     }
-                                    if (!match) {
-                                        matches = false;
-                                        break;
-                                    }
                                 }
                             }
+                            if (matches) {
+                                log.info(node.getServer() + " result matches!");
+                                result = respMessage;
+                                break;
+                            } else {
+                                log.info(node.getServer() + " result doesn't match");
+                            }
                         }
-                        if (matches) {
-                            log.info("Result matches!");
-                            result = respMessage;
-                            break;
-                        }
+                        currentIndex++;
                     }
-                } catch (Exception ignored) {
+                    if (matches) {
+                        break;
+                    }
                 }
-            }
-            if (result == null) {
-                log.info("Null result, use fallback instead");
-                result = fallback;
+                // Close channels
+                for (int i = 0; i < size; i++) {
+                    channels[i].disconnect();
+                    channels[i].close();
+                }
+                selector.close();
+            } catch (IOException e) {
+                log.warn("Failed to forward (chain): " + e);
             }
         } else {
             SocketAddress server;
             if (reqMessage.getQuestion().getType() == Type.AAAA) {
-                log.info("AAAA type query, use v6 server");
+                log.info("> AAAA type query, use v6 server");
                 server = Config.getV6Server();
             } else {
-                log.info("Other type query, use default");
+                log.info("> Other type query, use default");
                 server = Config.getDefaultServer();
             }
             // Query from server
-            Future<Message> respFuture = queryPool.submit(new QueryTask(reqMessage, server, Config.getTimeout()));
             try {
-                result = respFuture.get();
-            } catch (Exception ignored) {
+                DatagramSocket socket = new DatagramSocket();
+                socket.setSoTimeout(Config.getTimeout());
+                byte[] data = reqMessage.toWire();
+                socket.send(new DatagramPacket(data, data.length, server));
+                DatagramPacket packet = new DatagramPacket(new byte[4096], 4096);
+                socket.receive(packet);
+                socket.close();
+                result = new Message(packet.getData());
+            } catch (Exception e) {
+                log.warn("Failed to forward (single): " + e);
             }
         }
         if (result != null) {
